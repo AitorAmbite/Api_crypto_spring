46013a1a362204fed33103c7fc242c5bc08d4ab7
Aitor <Aitor@DESKTOP-TJFK767>
2021-02-22 18:18:48 +0100

    añadido modificacion ejercicio encriptacion

--- src/main/kotlin/com/example/ejemploPropio/CryptoController.kt
+++ src/main/kotlin/com/example/ejemploPropio/CryptoController.kt
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class CryptoController(private val cryptoRepository:CryptoRepository) {
 
+    val key = "estoEsMiLlave"
+
     @GetMapping("/coins")
     fun getCoins():List<CryptoCurrency>{
         return cryptoRepository.findAll()
@@ -16,5 +18,15 @@ class CryptoController(private val cryptoRepository:CryptoRepository) {
     @GetMapping("/coins/getMax/{number}")
     fun getMaxCoins(@PathVariable number: Int): MutableList<CryptoCurrency> {
         return cryptoRepository.findAll().subList(0,number)
+    }
+
+    @GetMapping("/coins/returnencrypted/{idEncrypt}")
+    fun getChyp(@PathVariable idEncrypt : String):String{
+        return encryptionController.encrypt(idEncrypt,key)
+    }
+
+    @GetMapping("/coins/returndecrypted/{idDecrypt}")
+    fun getDecrypted(@PathVariable idDecrypt : String):String{
+        return encryptionController.decrypt(idDecrypt,key)
     }
 }
---
+++ src/main/kotlin/com/example/ejemploPropio/encryptionController.kt
@@ -0,0 +1,32 @@
+package com.example.ejemploPropio
+
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+import java.security.MessageDigest
+
+class encryptionController {
+    companion object {
+        fun encrypt(textoEnString: String, llaveEnString: String): String {
+            val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+            cipher.init(Cipher.ENCRYPT_MODE, getKey(llaveEnString))
+            return URLEncoder.encode(Base64.getEncoder().encodeToString(cipher.doFinal(textoEnString.toByteArray(Charsets.UTF_8))),StandardCharsets.UTF_8)
+        }
+
+        fun decrypt(textoCifrado: String, llaveEnString: String): String {
+            val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+            cipher.init(Cipher.DECRYPT_MODE, getKey(llaveEnString))
+            return String(cipher.doFinal(Base64.getDecoder().decode(textoCifrado)))
+        }
+
+        fun getKey(llaveEnString: String): SecretKeySpec {
+            var llaveUtf8 = llaveEnString.toByteArray(Charsets.UTF_8)
+            val sha = MessageDigest.getInstance("SHA-1")
+            llaveUtf8 = sha.digest(llaveUtf8)
+            llaveUtf8 = llaveUtf8.copyOf(16)
+            return SecretKeySpec(llaveUtf8, "AES")
+        }
+    }
+}
